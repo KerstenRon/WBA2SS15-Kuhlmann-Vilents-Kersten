ab492da2d42502b59af49b505ee958d44cc7d373
Kr0oon <ronxd@ymail.com>
2015-04-22 14:39:26 +0200

    Hier ist die Lösung zur Aufgabe 1

---
+++ Aufgaben1/wolkenkratzer_liste.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var file = __dirname + '/wolkenkratzer.json';
+
+fs.readFile(file, function (err, data) {
+    if(err) {
+        console.log('Error:' + err);
+        return;
+    }
+        
+    var data = JSON.parse(data);
+    var i;    
+    for( i = 0; i < data.wolkenkratzer.length; i++) {
+        console.log("Name: " + data.wolkenkratzer[i].name + "\n" + "Stadt: " + data.wolkenkratzer[i].stadt + "\n" + "Hoehe: " + data.wolkenkratzer[i].hoehe + "\n----------------------------------");
+    }
+});
+
+var data = __dirname + '/my.json';
+
+
+fs.writeFile(data, JSON.stringify(file, null, 4), function(err){
+    if(err){
+        console.log('Error:' + err);
+        return;
+    } 
+    else {
+        console.log('It\'s saved!');
+    }
+});
