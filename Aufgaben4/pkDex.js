c6e447577eeb90a0b7dda2caa7f4e4af0eaebc59
Kr0oon <ronxd@ymail.com>
2015-05-16 22:06:13 +0200

    Verbesserte Version von pkDex.js, es ist jetzt möglich eine externe JSON per readFile() einzulesen

--- Aufgaben4/pkDex.js
+++ Aufgaben4/pkDex.js
@@ -5,78 +5,30 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var app = express();
 
-//Externe JSON-Dateien einlesen, damit man diese nicht mehr in der Serverimplementierung einfügen muss (funktioniert nicht!)
-//var content;
-//function readContent(callback) {
-//fs.readFile(file, function (err, data) {
-//    if(err) {
-//       return callback(err);
-//    }
-//        
-//    var data = JSON.parse(data);
-//    var data = data.pkdexGen1;
-//    callback(null, data);
-//});
-//};
+//Externe JSON-Dateien mit Node.js | FileSystem('fs') einlesen, damit man diese außerhalb der Serverimplementierung verwalten.
+var fs = require('fs');
+var file = __dirname + '/pkDexGen1.json'
+var content;
+function readContent(callback) {
+fs.readFile(file, function (err, data) {
+    if(err) {
+       return callback(err);
+    }
+    var data = JSON.parse(data);
+    callback(null, data);
+});
+};
 
-//readContent(function (err, content) {
-//console.log(content);
-//});
-
-//Das ist die zu verwendende JSON 'pkDex'
-var pkDex = [{"id":"001",
-                "name":"Bisasam",
-                "typ1":"Pflanze",
-                "typ2":"Gift",
-                "des":"Zwiebelkröte"},
-               {"id":"002",
-                "name":"Bisaknosp",
-                "typ1":"Pflanze",
-                "typ2":"Gift",
-                "des":"Knospenkröte"},
-               {"id":"003",
-                "name":"Bisaflor",
-                "typ1":"Pflanze",
-                "typ2":"Gift",
-                "des":"Ultra-BADASS-Kröte"},
-               {"id":"004",
-                "name":"Glumanda",
-                "typ1":"Feuer",
-                "typ2":"",
-                "des":"Feuerechse"},
-               {"id":"005",
-                "name":"Glutexo",
-                "typ1":"Feuer",
-                "typ2":"",
-                "des":"Feuerraptor"},
-               {"id":"006",
-                "name":"Glurak",
-                "typ1":"Feuer",
-                "typ2":"",
-                "des":"Feuerdrache"},
-               {"id":"007",
-                "name":"Schiggi",
-                "typ1":"Wasser",
-                "typ2":"",
-                "des":"Slowmo-Schildkröte"},
-               {"id":"008",
-                "name":"Schillog",
-                "typ1":"Wasser",
-                "typ2":"",
-                "des":"Babokröte"},
-               {"id":"009",
-                "name":"Turtok",
-                "typ1":"Wasser",
-                "typ2":"",
-                "des":"Tankkröte"}
-              ]
+readContent(function (err, content) {
+    pkDex=content.pkdexGen1;
+});
 
 //GET auf die Indexseite
 app.get('/', function(req, res) {
     //res.send('Willkommen in deinem persönlichen PkDex!');
     
-//Der Client kann sich jetzt aussuchen welchen Datentyp zurückgegeben werden soll (Hier auswahl zwischen den Accepttypes: 'html' und 'json')
-//Wird der Datentyp nicht akzeptiert wird der Fehlercode '406' zurückgegeben ('resource ... cannot be provided in a format specified in the Accept headers ...') 
+//Der Client kann sich jetzt aussuchen welchen Datentyp zurückgegeben werden soll (Hier auswahl zwischen den Accepttypes: 'html' und 'json').
+//Wird der Datentyp nicht akzeptiert wird der Fehlercode '406' zurückgegeben ('resource ... cannot be provided in a format specified in the Accept headers ...'). 
     var acceptType = req.accepts(['html', 'json']);
     switch(acceptType) {
         case 'html':
@@ -96,8 +48,8 @@ app.get('/pkDex', function(req, res){
 });
 //Per GET wird auf eine beliebige 'id' in der JSON 'pkDex' zugegriffen sollte ein Fehler auftreten (z.B.: id nicht vorhanden) wird der Statuscode '404' ausgegeben.
 app.get('/pkDex/:id', jsonParser, function(req, res){
-    var id = req.param.id;
-    console.log('Requested id is: '+id);
+    var id = req.params.id;
+    console.log("Die Angeforderte id ist: "+id);
     
     var filt = pkDex.filter(function(value, index, arr){
         return value.id == id;
@@ -118,7 +70,7 @@ app.get('/pkDex/:id', jsonParser, function(req, res){
     // "des":"string"}
 //...beachtet werden.
 app.post('/pkDex', jsonParser, function(req, res){
-    content.push(req.body);
+    pkDex.push(req.body);
     res.type('plain').send('Added!');
 });
 
