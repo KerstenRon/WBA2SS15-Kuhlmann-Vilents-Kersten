cc6a00a971425634a4ab142302659118c93dd296
Kr0oon <ronxd@ymail.com>
2015-08-17 11:13:37 +0200

    Back to the Light
    Konflikte sind reudig!!!!! GitHub auch!!!!

--- Aufgaben4/pkClient.js
+++ Aufgaben4/pkClient.js
@@ -7,10 +7,11 @@ var ejs = require('ejs');
 
 var app = express();
 
-app.get('/pkDex', jsonParser, function(req, res){
+app.get('/pkDex', jsonParser, function (req, res) {
+    'use strict';
     console.log('GET eingegangen!');
-    fs.readFile('./pkDex.ejs', {encoding: 'utf-8'} , function(err, filestring){
-        if(err){
+    fs.readFile('./pkDex.ejs', {encoding: 'utf-8'}, function (err, filestring) {
+        if (err) {
             throw err;
         } else {
             console.log('options gesetzt');
@@ -22,14 +23,13 @@ app.get('/pkDex', jsonParser, function(req, res){
                 headers: {
                     accept: 'application/json'
                 }
-            }
+            };
 
             //GET Request
-            var x = http.request(options, function(x) {
+            var x = http.request(options, function (x) {
                 console.log("Connected");
-                x.on('data', function(chunk){
+                x.on('data', function (chunk) {
                     //Verarbeitete Response
-<<<<<<< HEAD
 <<<<<<< HEAD
                     var userdata = JSON.parse('{ "pkDex": '+ chunk +'}');
                     console.log('Das ist der Chunk: ' + userdata.pkDex + ' Und das ist der Filestring: ' + filestring);
@@ -39,11 +39,6 @@ app.get('/pkDex', jsonParser, function(req, res){
                     console.log('Das ist der Chunk: ' + chunk + ' Und das ist der Filestring: ' + filestring);
                     console.log('Mischen und Rendern!');
 >>>>>>> origin/master
-=======
-                    var userdata = JSON.parse('{ "pkDex": '+ chunk +'}');
-                    console.log('Das ist der Chunk: ' + userdata.pkDex + ' Und das ist der Filestring: ' + filestring);
-                    console.log('Mischen und Randern!');
->>>>>>> 9df76c1c93451daac9e1c049bd68d3de49687934
                     var html = ejs.render(filestring, userdata);
                     res.setHeader('content-type', 'text/html');
                     res.writeHead(200);
@@ -57,10 +52,7 @@ app.get('/pkDex', jsonParser, function(req, res){
         }
     });
 });
-app.listen(3001, function(){
+app.listen(3001, function () {
+    'use strict';
     console.log("Server listens on Port 3001");
-<<<<<<< HEAD
 });
-=======
-})
->>>>>>> 9df76c1c93451daac9e1c049bd68d3de49687934
