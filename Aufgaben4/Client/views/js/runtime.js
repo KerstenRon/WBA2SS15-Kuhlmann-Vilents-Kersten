4ae233bcbda82a2ef2e30648d71317c6afa347b5
Leon <lvilents@gmail.com>
2015-09-01 22:30:08 +0200

    Implementation Clickfight #2
    Runtime erzeugt nun einen Button nur fürs Clicken, aber die Funktion
    greift nicht...was ist da los?!
    Runtime wurde mit Kommentaren ergänzt.

--- Aufgaben4/Client/views/js/runtime.js
+++ Aufgaben4/Client/views/js/runtime.js
@@ -1,3 +1,4 @@
+//Kleiner Countdown-Timer ("3...2...1...Los!").
 function startTimer() {
     'use strict';
     var i = 4, running = window.setInterval(function func() {
@@ -10,16 +11,15 @@ function startTimer() {
     }, 1000);
 }
 
+//Eigentlicher ClickFight-Timer.
 function clickFight() {
     'use strict';
-    var i = 61;
-   
-    var running = window.setInterval( function func() {
+    var i = 61, running = window.setInterval(function func() {
         $('.dl-text').text(--i);
         if (i === 0) {
             window.clearInterval(running);
-            $('#clicker').prop('id', 'start');
-            $('#start').text("Ende!");
+            //Ist die Zeit abgelaufen, wird der Start-Button wieder eingeblendet.
+            $('#start').fadeIn(200).text("Ende!");
         }
     }, 1000);
 }
@@ -28,22 +28,30 @@ $(document).ready(function () {
     'use strict';
     var count = 0, started = false;
     
+    //"Game Controller" - Hauptfunktion für den ClickFight.
     $('#start').click(function () {
+        //started fragt die Situation der Timer ab.
+        //false: "kleiner" Timer wurde noch nicht gestartet.
         if (!started) {
             console.log("Gleich geht's los...");
             $('#back').fadeOut(200);
             startTimer();
             started = true;
             console.log(started);
+        //true: "großer" Timer wurde noch nicht gestartet.
         } else {
             $('.dl-title').text("Click dich zum Sieg!");
-            $('#start').prop('id', 'clicker');
+            //Erzeugung eines eignes dafür gefertigten Click-Buttons.
+            $("<div class=\'dl-btn\' id=\'clicker\'>Click!</div>").insertAfter('#start');
+            //Der Start-Button wird für die Dauer des Spiels entfernt.
+            $('#start').fadeOut(200);
             clickFight();
         }
     });
     
+    //Clickfunktion für den Click-Button, welche allerdings nicht greift...was ist da los?!
     $('#clicker').click(function () {
-        count = count + 1;
+        count++;
         $('#clicker').text(count);
         console.log(count);
     });
